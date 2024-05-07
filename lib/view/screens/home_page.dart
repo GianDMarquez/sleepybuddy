1b7a2ef340d008736d927bb22a1bd22c5e3558cc
Gian David Marquez <giandmarquez@gmail.com>
2024-05-07 01:52:46 -0700

    what if i put my cat in the bar

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 import '../colors.dart';
-import 'package:sleeplist/view/screens/journal_details_page.dart';
-
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: primaryColorDark, 
-        title: Text('SleepyBuddy', style: TextStyle(color: primaryColorGray)),
-        actions: [
-        ],
-      ),
-      body: Text("Hello!", style: TextStyle(color: primaryColorGray))
-    );
+        appBar: AppBar(
+          backgroundColor: primaryColorDark,
+          title: Text('SleepyBuddy', style: TextStyle(color: primaryColorGray)),
+          actions: [
+
+           Image.asset(
+                'lib/images/logo_buddy.png', // Replace with your image asset
+                width: 72, // Set the desired width
+                height: 72, // Set the desired height
+              ),
+          ],
+        ),
+        body: Text("Hello!", style: TextStyle(color: primaryColorGray)));
   }
 }
