ff1393030d1e3992795bd2539880449f42c41ece
Gian David Marquez <giandmarquez@gmail.com>
2024-05-11 18:43:07 -0700

    add image
    Signed-off-by: Gian David Marquez <giandmarquez@gmail.com>

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -6,13 +6,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: primaryColorDark,
-          title: Text('SleepyBuddy', style: TextStyle(color: primaryColorGray, fontSize: 24)), centerTitle: true,
-          leading: Image.asset(
-                'lib/images/logo_buddy.png',
-              ),
+      appBar: AppBar(
+        backgroundColor: primaryColorDark,
+        title: Text('SleepyBuddy',
+            style: TextStyle(color: primaryColorGray, fontSize: 24)),
+        centerTitle: true,
+        leading: Image.asset(
+          'lib/images/logo_buddy.png',
         ),
-        body: Text("Hello!", style: TextStyle(color: primaryColorGray)));
+      ),
+      body: Center(
+          child: Image.asset("lib/images/logo_buddy_pressable.png"
+              //logo_name_n_buddy.png
+              //logo_buddy_pressable.png
+              )),
+    );
   }
 }
+
+        //logo_buddy_pressable.png
